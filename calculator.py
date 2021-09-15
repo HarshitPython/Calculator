cc367effcd3b8478c2fa0ea578fa7aa54c99b718
Harshit Shrivastava <88823245+HarshitPython@users.noreply.github.com>
2021-09-15 16:35:43 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,6 +9,7 @@ root.title('DataFlair - Calculator')
 
 #It keeps the track of current position on the input text field
 i = 0
+
 # Receives the digit as parameter and display it on the input field
 def get_variables(num):
 #     global i
